f44cc1474e44fa91320eb0b4464f15b1d5eacff6
Aa <aa@example.com>
2025-03-14 20:26:57 +0200

    updated english writing

--- framework/WordsWriting/WordsWriting.jsx
+++ framework/WordsWriting/WordsWriting.jsx
@@ -16,17 +16,13 @@ const WordsWriting = ({ words, courseId, lessonId }) => {
     setPreparedWords(shuffleArray(words));
   }, [words]);
 
-    return array;
-  }
-
-  prepareWords = () => {
-    this.setState({preparedWords: this.shuffleArray(this.props.words)});
   const shuffleArray = (array) => {
     let shuffledArray = [...array];
     for (let i = shuffledArray.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
     }
+    return shuffledArray;
   };
 
   const pushToWrongAnswers = (item) => {
